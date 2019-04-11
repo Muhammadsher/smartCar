e6310d992f4fc0d86cf37fdf41ac67547b853710
Mukhammadsher <mr.muhammadsher@mail.ru>
2019-04-11 12:37:20 +0500

    Ultrasonic

--- SmartCar/SmartCar/main.cpp
+++ SmartCar/SmartCar/main.cpp
@@ -1,14 +1,48 @@
-#include "Ultrasonic.h"
 #include "IR.h"
 #include "IR_Tracer.h"
 #include "Motor.h"
 #include <wiringPi.h>
 #include <iostream>
+#include <thread>
+#include <atomic>
+
+#define TRIG_PIN 28
+#define ECHO_PIN 29
 
 using namespace std;
+atomic<bool> stopBit{TRUE};
+
+void setUpUltrasonic() {
+	pinMode(TRIG_PIN, OUTPUT);
+	pinMode(ECHO_PIN, INPUT);
+}
+
+void getDistance() {
+
+	while (1) {
+		int start_time = 0, end_time = 0;
+		float distance = 0;
+
+		digitalWrite(TRIG_PIN, LOW);
+		delay(500);
+		digitalWrite(TRIG_PIN, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(TRIG_PIN, LOW);
+
+		while (digitalRead(ECHO_PIN) == 0);
+		start_time = micros();
+
+		while (digitalRead(ECHO_PIN) == 1);
+		end_time = micros();
+
+		distance = (end_time - start_time) / 29. / 2.;
+
+		stopBit = (distance > 35);
+		cout << "Distance: " << distance << endl;
+	}
+}
 
 int main() {
-	Ultrasonic uSensor;
 	Motor motor;
 	IR_Tracer tracer;
 	IR ir;
@@ -18,34 +52,20 @@ int main() {
 		return 1;
 	}
 
-	uSensor.setUp();
 	motor.initDCMotor();
 	tracer.setUp();
 	ir.setUp();
 
+	setUpUltrasonic();
+	thread th(getDistance);
 
 	while (1)
 	{
-
-		int IrRightBit = ir.RightIr();
-		int IrLeftBit = ir.LeftIr();
-		int tracerRight = tracer.rightIrTracer();
-		int tracerLeft = tracer.leftIrTracer();
-
-		int controlRight = IrRightBit | tracerRight;
-		int controlLeft = IrLeftBit | tracerLeft;
+		int controlRight = ir.RightIr() & tracer.rightIrTracer() & stopBit;
+		int controlLeft = ir.LeftIr() & tracer.leftIrTracer() & stopBit;
 
 		motor.control(controlRight, (controlLeft & ~controlRight), controlLeft, (controlRight & ~controlLeft));
 
-		//motor.control(IrRightBit, (IrLeftBit  & ~IrRightBit), IrLeftBit, (IrRightBit & ~IrLeftBit));
-
-		/*if (!uSensor.start())
-		{
-			motor.stopDCMotor();
-		}
-		else 
-		{
-		}*/
 
 	}
 
