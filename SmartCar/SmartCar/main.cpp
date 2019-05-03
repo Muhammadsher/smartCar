3241b2c12a8c3e026f2955915f9a603ea173b170
Mukhammadsher <mr.muhammadsher@mail.ru>
2019-05-03 14:39:30 +0500

    Temp

--- SmartCar/SmartCar/main.cpp
+++ SmartCar/SmartCar/main.cpp
@@ -1,76 +1,51 @@
+#include "Ultrasonic.h"
 #include "IR.h"
 #include "IR_Tracer.h"
 #include "Motor.h"
-#include "LaneTracerCam.h"
 #include <wiringPi.h>
 #include <iostream>
-#include <thread>
-#include <atomic>
 
 using namespace std;
-
-#define TRIG_PIN 28
-#define ECHO_PIN 29
-
-using namespace std;
-atomic<bool> stopBit{TRUE};
-
-void setUpUltrasonic() {
-	pinMode(TRIG_PIN, OUTPUT);
-	pinMode(ECHO_PIN, INPUT);
-}
-
-void getDistance() {
-
-	while (1) {
-		int start_time = 0, end_time = 0;
-		float distance = 0;
-
-		digitalWrite(TRIG_PIN, LOW);
-		delay(500);
-		digitalWrite(TRIG_PIN, HIGH);
-		delayMicroseconds(10);
-		digitalWrite(TRIG_PIN, LOW);
-
-		while (digitalRead(ECHO_PIN) == 0);
-		start_time = micros();
-
-		while (digitalRead(ECHO_PIN) == 1);
-		end_time = micros();
-
-		distance = (end_time - start_time) / 29. / 2.;
-
-		stopBit = (distance > 35);
-	}
-}
 
 int main() {
+	Ultrasonic uSensor;
 	Motor motor;
 	IR_Tracer tracer;
 	IR ir;
-	LaneTracerCam laneTracerCam;
-	
 
 	if (wiringPiSetup() == -1) {
 		cout << "Setup wiringPi failed !" << endl;
 		return 1;
 	}
 
+	uSensor.setUp();
 	motor.initDCMotor();
 	tracer.setUp();
 	ir.setUp();
 
-	setUpUltrasonic();
-	thread th(getDistance);
 
 	while (1)
 	{
-		int controlRight = ir.RightIr() & tracer.rightIrTracer() & stopBit;
-		int controlLeft = ir.LeftIr() & tracer.leftIrTracer() & stopBit;
+
+		int IrRightBit = ir.RightIr();
+		int IrLeftBit = ir.LeftIr();
+		int tracerRight = tracer.rightIrTracer();
+		int tracerLeft = tracer.leftIrTracer();
+
+		int controlRight = IrRightBit | tracerRight;
+		int controlLeft = IrLeftBit | tracerLeft;
 
 		motor.control(controlRight, (controlLeft & ~controlRight), controlLeft, (controlRight & ~controlLeft));
 
-		laneTracerCam.trace(motor);
+		//motor.control(IrRightBit, (IrLeftBit  & ~IrRightBit), IrLeftBit, (IrRightBit & ~IrLeftBit));
+
+		/*if (!uSensor.start())
+		{
+			motor.stopDCMotor();
+		}
+		else 
+		{
+		}*/
 
 	}
 
